2e526b322ba760bfbc5e04396ff1907ec0098710
Milton Bolonha <miltonsiedler@gmail.com>
2026-01-29 08:34:58 -0300

    git init

---
+++ core/src/pages/_document.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+import logos from "@/content/settings/logos.json";
+import theme from "@/content/settings/theme.json";
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const originalRenderPage = ctx.renderPage;
+
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) => <App {...props} />,
+      });
+
+    const initialProps = await Document.getInitialProps(ctx);
+    return {
+      ...initialProps,
+    };
+  }
+
+  render() {
+    return (
+      <Html lang="en-US">
+        <Head>
+          <link
+            rel="icon prefetch"
+            href={logos.faviconLogo || "/brandimages/favicon.png"}
+            sizes="any"
+          />
+        </Head>
+        <body
+          className={`boilerplate-times theme-${theme.generalThemeSettings.themeStyle}`}
+        >
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
